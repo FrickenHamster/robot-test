037be13e7aa492aa84164466d7df139d93f2114b
Alexander Yan <frickenhamster@gmail.com>
2018-03-30 02:19:44 -0700

    inital commit

---
+++ Assets/Scripts/RoboScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float hAxis = Input.GetAxis("Horizontal");
+		this.transform.Translate(hAxis * 5 * Time.deltaTime, 0, 0, Space.World);
+	}
+}
