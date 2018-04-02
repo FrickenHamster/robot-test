52e4cded5618a35618201605458025bf15a0217a
Alexander Yan <frickenhamster@gmail.com>
2018-04-02 13:29:06 -0700

    add menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public Button StartButton;
+
+	void Start () {
+		StartButton.onClick.AddListener(StartGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartGame() {
+		SceneManager.LoadScene("Game");
+	}
+}
